17253f937406e901f107a953d6e8c9ff257e810d
Inderby <youk8253@gmail.com>
2024-04-29 00:39:51 +0900

    #5 - Getter패턴으로 변경

--- api/src/main/java/org/delivery/api/common/error/ErrorCode.java
+++ api/src/main/java/org/delivery/api/common/error/ErrorCode.java
@@ -1,9 +1,11 @@
 package org.delivery.api.common.error;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
+@Getter
 public enum ErrorCode implements Error{
     OK(200, 200, "성공"),
     BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), 400, "잘못된 요청"),
@@ -14,19 +16,4 @@ public enum ErrorCode implements Error{
     private final Integer httpStatusCode;
     private final Integer errorCode;
     private final String description;
-
-    @Override
-    public Integer getHttpStatusCode() {
-        return this.httpStatusCode;
-    }
-
-    @Override
-    public Integer getErrorCode() {
-        return this.errorCode;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
 }
--- api/src/main/java/org/delivery/api/common/error/UserErrorCode.java
+++ api/src/main/java/org/delivery/api/common/error/UserErrorCode.java
@@ -1,12 +1,14 @@
 package org.delivery.api.common.error;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 /**
  * User의 경우 1000번대 에러코드 사용
  */
 @AllArgsConstructor
+@Getter
 public enum UserErrorCode implements Error{
     USER_NOT_FOUND(400, 1404, "사용자를 찾을 수 없다."),
     ;
@@ -14,19 +16,4 @@ public enum UserErrorCode implements Error{
     private final Integer httpStatusCode;
     private final Integer errorCode;
     private final String description;
-
-    @Override
-    public Integer getHttpStatusCode() {
-        return this.httpStatusCode;
-    }
-
-    @Override
-    public Integer getErrorCode() {
-        return this.errorCode;
-    }
-
-    @Override
-    public String getDescription() {
-        return this.description;
-    }
 }
