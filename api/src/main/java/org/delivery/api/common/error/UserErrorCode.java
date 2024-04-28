2dc01394ee9a9d9cd94ef690e4ad40d3c7c4b751
Inderby <youk8253@gmail.com>
2024-04-29 00:21:59 +0900

    #5 - api 공통 spec 정의 및 에러 코드 정의
    response에 대한 기본적인 모양새와 발생할 수 있는 간단한 에러들에 대해 정의하고 이를 result 부분으로 따로 묶어서 처리함.

---
+++ api/src/main/java/org/delivery/api/common/error/UserErrorCode.java
@@ -0,0 +1,32 @@
+package org.delivery.api.common.error;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+/**
+ * User의 경우 1000번대 에러코드 사용
+ */
+@AllArgsConstructor
+public enum UserErrorCode implements Error{
+    USER_NOT_FOUND(400, 1404, "사용자를 찾을 수 없"),
+    ;
+
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
+    private final String description;
+
+    @Override
+    public Integer getHttpStatusCode() {
+        return this.httpStatusCode;
+    }
+
+    @Override
+    public Integer getErrorCode() {
+        return this.errorCode;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+}
